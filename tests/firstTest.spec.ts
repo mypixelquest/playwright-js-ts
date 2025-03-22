9a9f923acc066e71bd6b80e4ab86c5398a55bad5
Yash <mypixelquest@gmail.com>
2025-03-22 18:31:26 +0530

    Refactor Playwright configuration and enhance first test suite with navigation steps

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,16 +1,33 @@
 import { test } from "@playwright/test";
 
-test("the first test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
-  await page.getByText("Forms").click();
-  await page.getByText("Form Layouts").click();
 });
 
-// // Test suite
-// test.describe("test suite", () => {
-//   test("the third test", async ({ page }) => {
-//     await page.goto("http://localhost:4200/");
-//     await page.getByText("Forms").click();
-//     await page.getByText("Form Layouts").click();
-//   });
-// });
+test.describe("suite1", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts").click();
+  });
+
+  test("navigate to the form layouts", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("navigate to the datepicker page", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
+});
+
+test.describe.only("suite2", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+
+  test("test3 ", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("test4", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
+});

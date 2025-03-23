cd768daf578c235117e69fb1837b6711df106538
Yash <mypixelquest@gmail.com>
2025-03-23 18:50:37 +0530

    Add examples of objects and arrays in JavaScript

---
+++ section-2-javascript-fundamentals/09-objects-arrays.js
@@ -0,0 +1,17 @@
+// objects
+
+var customer = {
+  firstName: "Tom",
+  lastName: "Hanks",
+};
+console.log(customer.firstName);
+console.log(customer["lastName"]);
+console.log(`${customer.firstName} ${customer.lastName}`);
+
+// arrays
+var colors = ["red", "green", "blue"];
+var cars = ["BMW", "Audi", "Mercedes"];
+console.log(colors[0]);
+console.log(cars);
+console.log(cars.length);
+console.log(cars[2]);

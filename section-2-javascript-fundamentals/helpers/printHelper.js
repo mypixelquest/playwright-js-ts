f700576ca48fc29b561e757c2f4ee83abca47725
Yash <mypixelquest@gmail.com>
2025-03-23 21:11:52 +0530

    Add CustomerDetails class with methods to print first and last names

--- section-2-javascript-fundamentals/helpers/printHelper.js
+++ section-2-javascript-fundamentals/helpers/printHelper.js
@@ -1,3 +1,17 @@
 export function printAge(age) {
   console.log(`Age: ${age}`);
 }
+
+// 15-class-methods
+
+class CustomerDetails {
+  printFirstName(printFirstName) {
+    console.log(`First Name: ${printFirstName}`);
+  }
+
+  printLastName(printLastName) {
+    console.log(`Last Name: ${printLastName}`);
+  }
+}
+
+export const customerDetails = new CustomerDetails();
